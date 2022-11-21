c633f547482f810486ab7b9c041a8d5784f72f3d
faaiz777 <faaiz0076@gmail.com>
2022-11-22 00:44:05 +0100

    Changed Batch File Name

--- TestRunner/Batch-File-Examples/RunTestRunner.bat
+++ TestRunner/Batch-File-Examples/RunTestRunner.bat
@@ -1,17 +1,13 @@
 :: TIA Portal Test Runner Console
 :: Execute test cases and style guide checks with TIA Portal TestSuite
 
-cd C:\jenkins
-git clone https://github.com/faaiz777/ThesisProject-TIA-Portal.git
-
-cd C:\jenkins\ThesisProject-TIA-Portal
-
-
 @echo off
-set TESTRUNNER_FOLDER="C:\jenkins\ThesisProject-TIA-Portal\TestRunner"
-set TIAP_PROJ="C:\jenkins\ThesisProject-TIA-Portal\DemoProject_V17.ap17"
-set REPORT_FILE_PATH="C:\jenkins\ThesisProject-TIA-Portal\Report.xml"
-set LOGGING_FILE_PATH="C:\jenkins\ThesisProject-TIA-Portal\runner.log"
+set TESTRUNNER_FOLDER="C:\jenkins\workspace\test\TestRunner"
+C:\jenkins\workspace\test
+set TIAP_PROJ="C:\jenkins\workspace\test\DemoProject_V17.ap17"
+set REPORT_FILE_PATH="C:\jenkins\workspace\Report.xml"
+:: set REPORT_FILE_PATH="C:\jenkins\ThesisProject-TIA-Portal\Report.xml"
+set LOGGING_FILE_PATH="C:\jenkins\workspace\test\runner.log"
 
 :: change to the Test Runner Console executable folder
 pushd %TESTRUNNER_FOLDER%
