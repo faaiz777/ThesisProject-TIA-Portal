2ac6087234398c8ec60781171aabc4061ff00f60
faaiz777 <faaiz0076@gmail.com>
2022-11-22 00:59:44 +0100

    Changed Batch File Name

--- TestRunner/Batch-File-Examples/RunTestRunner.bat
+++ TestRunner/Batch-File-Examples/RunTestRunner.bat
@@ -12,7 +12,7 @@ set LOGGING_FILE_PATH="C:\jenkins\workspace\test\runner.log"
 :: change to the Test Runner Console executable folder
 pushd %TESTRUNNER_FOLDER%
 @echo on
-
+C:\jenkins\workspace\test
 :: Call Test Runner
 :: run all test cases in project
 :: run only SiemensProgrammingStyleguide style guide in project
@@ -21,6 +21,6 @@ Siemens.TestRunner.Console.exe ^
  --report %REPORT_FILE_PATH% ^
  --format=JUNIT_HUDSON ^
  --project=%TIAP_PROJ% ^
- --test-style SiemensProgrammingStyleGuideRules ^
+ --test-style StyleguideRulesets.xml ^
  --test-application all ^
  --test-application-plc-scope PLC_1
